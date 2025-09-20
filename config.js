8c308cf4abbf5c7ec0db3cf22d183a9fbb2e826d
Mr-rabbit-xd <bihh5539@gmail.com>
2025-09-20 21:37:53 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  TELEGRAM_TOKEN: '8216808317:AAHhpWkwf7TlpLO0eQZmEGQeHH49llaupZ0',
+  ADMIN_ID: 7616539095, // Telegram ID for admin
+
+  FIREBASE: {
+    project_info: {
+      project_number: "204316192596",
+      firebase_url: "https://vcfweb-9bd1f-default-rtdb.firebaseio.com",
+      project_id: "vcfweb-9bd1f",
+      storage_bucket: "vcfweb-9bd1f.firebasestorage.app"
+    },
+    client: [
+      {
+        client_info: {
+          mobilesdk_app_id: "1:204316192596:android:5b5d6052167c5dd270a2c1",
+          android_client_info: {
+            package_name: "com.Vcf.mrrabbit"
+          }
+        },
+        oauth_client: [],
+        api_key: [
+          {
+            current_key: "AIzaSyDOJRWTrO3A7_0SqvBxsCTt6bkNo8pV-bc"
+          }
+        ],
+        services: {
+          appinvite_service: {
+            other_platform_oauth_client: []
+          }
+        }
+      }
+    ],
+    configuration_version: "1"
+  }
+};
